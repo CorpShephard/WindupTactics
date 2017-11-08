bfa0cad1d2207c6cc66a1a82f9915a5c8232ba03
Woody Barrett <Rofelen@gmail.com>
2017-11-07 17:45:42 -0800

    RTS Camera Zoom
    Added basic zoom.  TODO: Clamp Zoom.

--- Assets/Scripts/View/RTSCameraController.cs
+++ Assets/Scripts/View/RTSCameraController.cs
@@ -4,9 +4,17 @@ public class RTSCameraController : MonoBehaviour {
 
     public float panSpeed = 10f;
     public float panBorderThickness = 10f;
-	
-	// Update is called once per frame
-	void Update () {
+    public Vector2 panLimit;
+    public float cameraSize;
+    public float zoomSpeed = 1f;
+
+    private Camera camera;
+
+
+    //public float zoomSpeed = 20f; Zoom in and out
+
+    // Update is called once per frame
+    void Update () {
 
         Vector3 futurePos = transform.position;
 
@@ -29,6 +37,18 @@ public class RTSCameraController : MonoBehaviour {
         {
             futurePos.x += panSpeed * Time.deltaTime;
         }
+
+        camera = (Camera)this.GetComponent("Camera");
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        camera.orthographicSize += zoom * zoomSpeed * 100f * Time.deltaTime;
+
+
+
+
+
+
+        futurePos.x = Mathf.Clamp(futurePos.x, -panLimit.x, panLimit.x); // Clamp the raw position value based on editor defined limits.
+        futurePos.y = Mathf.Clamp(futurePos.y, -panLimit.y, panLimit.y);
 
         transform.position = futurePos;
     }
