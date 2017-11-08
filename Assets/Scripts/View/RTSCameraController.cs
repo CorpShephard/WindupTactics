9d41c2d68a7c9428fbff553fcd7a2b8c929469a5
Woody Barrett <Rofelen@gmail.com>
2017-11-07 17:06:35 -0800

    MVC Folders Plus RTS Camera
    Added Model-View-Controller layout folders and basic RTS camera control.

---
+++ Assets/Scripts/View/RTSCameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class RTSCameraController : MonoBehaviour {
+
+    public float panSpeed = 10f;
+    public float panBorderThickness = 10f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 futurePos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height-panBorderThickness) // Check for cardinal movement keys
+        {
+            futurePos.y += panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            futurePos.y -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            futurePos.x -= panSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            futurePos.x += panSpeed * Time.deltaTime;
+        }
+
+        transform.position = futurePos;
+    }
+}
